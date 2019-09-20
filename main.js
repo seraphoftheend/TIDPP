ac3ddd120aafd96c6290da301c294a6eda315b29
seraphoftheend <alexeysv96@gmail.com>
2019-09-20 04:36:36 +0300

    Changed 'main.js'

--- main.js
+++ main.js
@@ -6,11 +6,8 @@
   <script>
     alert( 'Hello, world!' );
 	alert( 'Hello, world!' );
-<<<<<<< HEAD
 	alert( 'Hello, Milky Way!' );
-=======
 	alert( 'Hi, world!' );
->>>>>>> javascript
   </script>
 
 </body>
